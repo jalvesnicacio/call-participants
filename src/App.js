2f86aae803fd0bb0b911ca26979399c888290fe6
Jalves Nicacio <jalves.nicacio@gmail.com>
2022-11-29 23:44:01 -0500

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle";
 import logo from './logo.svg';
 import './App.css';
 
